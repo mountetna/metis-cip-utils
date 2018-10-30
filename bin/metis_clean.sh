2dfdcef0bc00797a759a940bd64fe384baf53eb5
Jason Cater <jasondcater@gmail.com>
2018-10-30 10:14:04 -0700

    Adding a small script to clean up old metis scans.

---
+++ bin/metis_clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This will clean up the old metis scan files. This script won't be required in
+# the future.
+#
+# The command call should be...
+# ./metis_clean.sh [input_file] > output_file.csv
+
+while IFS=$'\t' read -r -a tmp ; do
+do
+  printf "%s,%s,\n" "${p[0]}" "${p[1]}" 
+done < "$1"
