0dfbed0136e56b79517c0cc54c07ca200d6cf5ab
Jason Cater <jasondcater@gmail.com>
2018-10-30 10:45:39 -0700

    Adding a check for webscrape files.

--- bin/metis_scan.sh
+++ bin/metis_scan.sh
@@ -5,10 +5,24 @@
 # ./metis_scan.sh [directory] > [output_scan.csv]
 
 while read line; do
+
   file_data=($line)
   size=${file_data[0]}
   name=${file_data[1]}
-  sum="$(md5sum $name 2>/dev/null)"
-  sum=${sum%% *}     # Strip off filename to get sum only.
-  echo "$size,$name,$sum"
+
+  if ! [[ \
+    $name == *"/"  || \
+    $name == *"?C=M;O=D" || \
+    $name == *"?C=M;O=A" || \
+    $name == *"?C=N;O=D" || \
+    $name == *"?C=N;O=A" || \
+    $name == *"?C=S;O=D" || \
+    $name == *"?C=S;O=A" || \
+    $name == *"?C=D;O=D" || \
+    $name == *"?C=D;O=A" \
+  ]]; then
+    sum="$(md5sum $name 2>/dev/null)"
+    sum=${sum%% *}     # Strip off filename to get sum only.
+    echo "$size,$name,$sum"
+  fi
 done <<<"$(find $1 -type f -printf "%s %p\n")"
